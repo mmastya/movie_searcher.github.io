b53de16e12505c9409b60190c228c4c91f6f5898
Borodina <asya07200@gmail.com>
2020-03-22 18:04:38 +0300

    add some updates

---
+++ precache-manifest.aff13d234eae9a26d1982369aabed1ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449e29d8f55e3cdc4f9515ad1b93002f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2479283bd5627e9f5651",
+    "url": "/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "09f8a0e0f1db5c2c5da1",
+    "url": "/static/css/main.b19aff5d.chunk.css"
+  },
+  {
+    "revision": "2479283bd5627e9f5651",
+    "url": "/static/js/2.21ee37f9.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.21ee37f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f8a0e0f1db5c2c5da1",
+    "url": "/static/js/main.5a30480d.chunk.js"
+  },
+  {
+    "revision": "6192a453e7fad1769497",
+    "url": "/static/js/runtime-main.c2da20d9.js"
+  }
+]);
